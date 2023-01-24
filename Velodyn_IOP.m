183db236c15b6e72c1c27f1f1e3be6fd22a51dc6
ZzZifanZ <zifan.zhangs@gmail.com>
2023-01-23 21:40:30 -0700

    test

---
+++ Velodyn_IOP.m
@@ -0,0 +1,18 @@
+
+
+run1 = main(2,0,'test.csv','C:\Users\zifan\OneDrive\Desktop\School\ENGO 500\devkit_raw_data\devkit\2011_09_26_drive_0027_sync','2011_09_26','0027');
+
+
+
+function a = main(cam,frame,str,basep,date,drive)
+
+    fullfile(pwd, basep)
+    %'C:\Users\zifan\OneDrive\Desktop\School\ENGO 500\devkit_raw_data\devkit\2011_09_26_drive_0027_sync')
+
+    a = 1;
+    [velo_img, pt] = run_demoVelodyne(append(basep,'\',date,'\',date,'_','drive','_',drive,'_sync'),...
+    append(basep ,'\',date),frame,cam);
+
+    writematrix(velo_img, str);
+
+end 
